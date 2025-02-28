67e33609b60bc0af53f0cf2b0eaf39a64afe90e7
Monzini93 <95325273+Monzini93@users.noreply.github.com>
2025-02-28 16:17:51 -0300

    Add report generation script with directory management and Excel export functionality

---
+++ gera_relatorios.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import os
+from openpyxl import load_workbook
+
+dir_principal = 'C:\Users\yurim\Documents\python'
+#dir_principal = '/Users/Leonardo/Desktop/Tocalivros Dropbox/Campsoft/Geral/Relatórios/provedores'
+
+### Verifica se o diretório existe e o cria se não existir. ###
+def criar_diretorio(caminho):
+    if not os.path.exists(caminho):
+        os.makedirs(caminho)
+        print(f"Diretório criado: {caminho}")
+    else:
+        print(f"O diretório já existe: {caminho}")
+
+
+# CRIA RELATÓRIO BRUTO
+def gera_latorio_bruto(dir_provedor,arquivo_nome,df):
+    criar_diretorio(f"{dir_provedor}/bruto/")
+    df.to_excel(f"{dir_provedor}/bruto/{arquivo_nome}", index=False)
+
+
+# CRIA RELATÓRIO DETALHADO
+def gera_latorio_detalhado(dir_provedor,arquivo_nome,df):
+    #Seleciona apenas as colunas necessárias
+    df_detalhado = df[['ano', 'mes','provedor_nome','conteudista_produto_nome','nome','cpf_cnpj','email','data_inicio','data_final']]
+    criar_diretorio(f"{dir_provedor}/detalhado/")
+
+    #Salva excel
+    df_detalhado.to_excel(f"{dir_provedor}/detalhado/{arquivo_nome}", index=False)
+
+    # Abre o arquivo Excel com openpyxl
+    workbook = load_workbook(f"{dir_provedor}/detalhado/{arquivo_nome}")
+    worksheet = workbook.active
+
+    # Define o tamanho das colunas
+    col_widths = {
+        'A': 5, # ano
+        'B': 5, # mes
+        'C': 20, # provedor_nome
+        'D': 25, # conteudista_produto_nome
+        'E': 50, # nome
+        'F': 17, # cpf_cnpj
+        'G': 50, # email
+        'H': 18, # data_inicio
+        'I': 18 # data_inicio
+    }
+
+    # Ajusta o tamanho das colunas
+    for col, width in col_widths.items():
+        worksheet.column_dimensions[col].width = width
+
+    # Salva o arquivo Excel com as colunas redimensionadas
+    workbook.save(f"{dir_provedor}/detalhado/{arquivo_nome}")
+    workbook.close()
+
+
+# CRIA RELATÓRIO RESUMIDO
+def gera_latorio_resumido(dir_provedor,arquivo_nome,df):
+    df_resumido = (
+        df.groupby(['ano', 'mes', 'conteudista_produto_nome'])
+        .agg({'id_provedor_usuario': pd.Series.nunique})
+        .reset_index()
+        .rename(columns={'id_provedor_usuario': 'assinaturas'})
+    )
+    criar_diretorio(f"{dir_provedor}/resumido/")
+    df_resumido.to_excel(f"{dir_provedor}/resumido/{arquivo_nome}", index=False)
+
+    # Abre o arquivo Excel com openpyxl
+    workbook = load_workbook(f"{dir_provedor}/resumido/{arquivo_nome}")
+    worksheet = workbook.active
+
+    # Define o tamanho das colunas
+    col_widths = {
+        'A': 5, # ano
+        'B': 5, # mes
+        'C': 26, # conteudista_produto_nome
+        'D': 10, # assinaturas
+    }
+
+    # Ajusta o tamanho das colunas
+    for col, width in col_widths.items():
+        worksheet.column_dimensions[col].width = width
+        
+    # Salva o arquivo Excel com as colunas redimensionadas
+    workbook.save(f"{dir_provedor}/resumido/{arquivo_nome}")
+    workbook.close()
+
+
+def gera_latorio(id_provedor,nome_provedor,df):
+    try:
+        ano = df.iloc[0]['ano']
+        mes = df.iloc[0]['mes']
+        dir_provedor = f"{dir_principal}/{id_provedor}"
+        arquivo_nome = f"{ano}.{mes}-{nome_provedor}.xlsx"
+
+        gera_latorio_bruto(dir_provedor,arquivo_nome,df)
+        gera_latorio_detalhado(dir_provedor,arquivo_nome,df)
+        gera_latorio_resumido(dir_provedor,arquivo_nome,df)
+
+    except:
+        pass
